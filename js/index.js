5a999f279ac095c447ece37c21846d651253cac8
lena <lenok-cotik@yandex.ru>
2017-08-16 23:05:34 +0300

    Add animation for text

--- js/index.js
+++ js/index.js
@@ -6,29 +6,48 @@ var ImagesForSlider = (function () {
     return ImagesForSlider;
 }());
 var imagesForSlider = [
-    new ImagesForSlider('./images/products/1.png', 'Some text 1'),
+    new ImagesForSlider('./images/products/1.png', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam, libero'),
     new ImagesForSlider('./images/products/2.png', 'Some text 2'),
-    new ImagesForSlider('./images/products/3.png', 'Some text 3'),
+    new ImagesForSlider('./images/products/3.png', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam, libero'),
     new ImagesForSlider('./images/products/4.png', 'Some text 4'),
 ];
 var sliderImage = document.getElementById('sliderImage');
+var sliderText = document.getElementById('sliderText');
 var currentImageIndex = 0;
 function sliderInit() {
     sliderImage.src = imagesForSlider[currentImageIndex].src;
+    sliderText.innerText = imagesForSlider[currentImageIndex].text;
 }
 function setSliderImage() {
     sliderImage.className += "fadeOut";
     setTimeout(function () {
         sliderImage.src = imagesForSlider[currentImageIndex].src;
+        sliderText.innerText = imagesForSlider[currentImageIndex].text;
+        sliderText.style.left = 'auto';
+        sliderText.style.right = 'auto';
         sliderImage.className = "";
     }, 500);
 }
 function previousImage() {
-    currentImageIndex = currentImageIndex - 1 < 0 ? imagesForSlider.length - 1 : currentImageIndex - 1;
+    currentImageIndex = --currentImageIndex < 0 ? imagesForSlider.length - 1 : currentImageIndex;
+    var pos = 0;
+    var animation = setInterval(function () {
+        sliderText.style.left = ++pos + '%';
+    }, 20);
+    setTimeout(function () {
+        clearInterval(animation);
+    }, 500);
     setSliderImage();
 }
 function nextImage() {
     currentImageIndex = (currentImageIndex + 1) % imagesForSlider.length;
+    var pos = 0;
+    var animation = setInterval(function () {
+        sliderText.style.right = ++pos + '%';
+    }, 20);
+    setTimeout(function () {
+        clearInterval(animation);
+    }, 500);
     setSliderImage();
 }
 sliderInit();
--- ts/index.ts
+++ ts/index.ts
@@ -9,34 +9,62 @@ class ImagesForSlider {
 }
 
 let imagesForSlider: ImagesForSlider[] = [
-    new ImagesForSlider('./images/products/1.png', 'Some text 1'),
+    new ImagesForSlider('./images/products/1.png', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam, libero'),
     new ImagesForSlider('./images/products/2.png', 'Some text 2'),
-    new ImagesForSlider('./images/products/3.png', 'Some text 3'),
+    new ImagesForSlider('./images/products/3.png', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam, libero'),
     new ImagesForSlider('./images/products/4.png', 'Some text 4'),
 ];
 
 let sliderImage: HTMLImageElement = <HTMLImageElement>document.getElementById('sliderImage');
+let sliderText = document.getElementById('sliderText');
+
 let currentImageIndex = 0;
 
 function sliderInit() {
     sliderImage.src = imagesForSlider[currentImageIndex].src;
+    sliderText.innerText = imagesForSlider[currentImageIndex].text;
 }
 
 function setSliderImage() {
     sliderImage.className += "fadeOut";
     setTimeout(() => {
         sliderImage.src = imagesForSlider[currentImageIndex].src;
+
+        sliderText.innerText = imagesForSlider[currentImageIndex].text;
+        sliderText.style.left = 'auto';
+        sliderText.style.right = 'auto';
+
         sliderImage.className = "";
     }, 500);
 }
 
 function previousImage() {
-    currentImageIndex = currentImageIndex - 1 < 0 ? imagesForSlider.length - 1 : currentImageIndex - 1;
+    currentImageIndex = --currentImageIndex < 0 ? imagesForSlider.length - 1 : currentImageIndex;
+
+    let pos = 0;
+    let animation = setInterval(() => {
+        sliderText.style.left = ++pos + '%';
+    }, 20);
+
+    setTimeout(() => {
+        clearInterval(animation);
+    }, 500);
+
     setSliderImage();
 }
 
 function nextImage() {
     currentImageIndex = (currentImageIndex + 1) % imagesForSlider.length;
+
+    let pos = 0;
+    let animation = setInterval(() => {
+        sliderText.style.right = ++pos + '%';
+    }, 20);
+
+    setTimeout(() => {
+        clearInterval(animation);
+    }, 500);
+
     setSliderImage();
 }
 
