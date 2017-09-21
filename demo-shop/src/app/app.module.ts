2119c0bc8f6ebc5a219033fce640eead67673f7e
Tuseeva Elena <etuseeva@griddynamics.com>
2017-09-21 18:28:51 +0300

    ...

--- demo-shop/src/app/app.module.ts
+++ demo-shop/src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ErrorPagesComponent } from './error-pages/error-pages.component';
-import { ProductsDetailsPageComponent } from './main/products-details-page/products-details-page.component';
-import { ProductsListPageComponent } from './main/products-list-page/products-list-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import {AppComponent} from './app.component';
+import {ErrorPagesComponent} from './error-pages/error-pages.component';
+import {ProductsDetailsPageComponent} from './main/products-details-page/products-details-page.component';
+import {ProductsListPageComponent} from './main/products-list-page/products-list-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
 import {routing} from "./app.routing";
 import {FormsModule} from "@angular/forms";
 import {ErrorHandlerService} from "./services/error-handler.service";
-import { MainComponent } from './main/main.component';
-import { ProductCardComponent } from './main/product-card/product-card.component';
-import { HeaderComponent } from './header/header.component';
+import {MainComponent} from './main/main.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {ProductDetailCardComponent} from './main/products-details-page/product-detail-card/product-detail-card.component';
+import {ProductListCardComponent} from "./main/products-list-page/product-list-card/product-list-card.component";
 
 @NgModule({
   declarations: [
@@ -21,8 +23,10 @@ import { HeaderComponent } from './header/header.component';
     ProductsListPageComponent,
     LoginPageComponent,
     MainComponent,
-    ProductCardComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ProductDetailCardComponent,
+    ProductListCardComponent
   ],
   imports: [
     BrowserModule,
@@ -34,4 +38,5 @@ import { HeaderComponent } from './header/header.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ demo-shop/src/app/main/products-list-page/product-list-card/product-list-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-card',
+  templateUrl: './product-list-card.component.html',
+  styleUrls: ['./product-list-card.component.scss']
+})
+export class ProductListCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
