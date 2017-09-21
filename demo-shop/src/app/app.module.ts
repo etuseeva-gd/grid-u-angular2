1e6a1ac395d6b7fee12d6d721d89514922885004
Tuseeva Elena <etuseeva@griddynamics.com>
2017-09-21 16:54:23 +0300

    Add components

--- demo-shop/src/app/app.module.ts
+++ demo-shop/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ErrorPagesComponent } from './error-pages/error-pages.component';
+import { ProductsDetailsPageComponent } from './products-details-page/products-details-page.component';
+import { ProductsListPageComponent } from './products-list-page/products-list-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPagesComponent,
+    ProductsDetailsPageComponent,
+    ProductsListPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
